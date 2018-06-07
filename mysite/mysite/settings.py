75627e769604519afff49d6aba41febad5bb3f7d
Marcos <m.ortega@hotmail.ca>
2018-06-06 22:46:00 -0400

    Allow Pythonanywhere Host

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = ')*_e750em3uy4n!p5@68^gf7owis97ta9e02_x)f&&%qhe0_bx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ortegam4.pythonanywhere.com']
 
 
 # Application definition
